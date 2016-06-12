133e4a4e19df8390b454c9caad827cbfb7b06079
Aleksey <aleksey.zakharov@gmail.com>
2016-06-12 14:54:32 -0700

    added password_digest to Users DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
+
+  #we need to downcase before we save, saving is case sensitive
+  before_save { self.email = email.downcase }
+
   validates :name,  presence: true, length: { maximum: 50 }
+
+  #this is the 'basic' regex which will give us proper email format
+  #we could make it more complex but we dont need to atm
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  #we need email uniquness so that we can search db and not have collisions
   validates :email, presence: true, length: { maximum: 255 },
               format: {with: VALID_EMAIL_REGEX },
               uniqueness: { case_sensitive: false}
