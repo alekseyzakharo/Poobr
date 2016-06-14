acdc78251325a050e984fbfbd884fce8ee3cbe58
Aleksey <aleksey.zakharov@gmail.com>
2016-06-13 21:53:59 -0700

    basic accounts work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) #will change
     if @user.save
+      log_in @user
       flash[:success] = "Welcome New User"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get 'sessions/new'
+
   get 'welcome/index'
 
   #get 'users/new'
