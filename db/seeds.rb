054213bf1c53c4db10e12b2ce497cf70cb2529a1
Aleksey <aleksey.zakharov@gmail.com>
2016-06-17 12:56:10 -0700

    fixed user level

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(name: 'admin', email: 'admin@admin.com', password: 'admin',
-                        password_confirmation: 'admin', level: 1)
+                        password_confirmation: 'admin', level: 0)
